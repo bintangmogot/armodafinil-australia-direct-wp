95a9150e2080100478a05d8fa610635f1b1a72e7
bintangmogot <harunbintangmogot07@gmail.com>
2026-08-30 00:11:41 +0800

    fix: correctly hook acf_add_options_page in functions.php

--- functions.php
+++ functions.php
@@ -26,7 +26,17 @@ require get_template_directory() . '/inc/acf-fields.php';
 require_once get_template_directory() . '/inc/cpt.php';
 
 
-if( function_exists('acf_add_options_page') ) { acf_add_options_page(array('page_title' => 'Theme General Settings', 'menu_title' => 'Theme Settings', 'menu_slug' => 'theme-general-settings', 'capability' => 'edit_posts', 'redirect' => false)); }
+add_action('acf/init', function() {
+    if( function_exists('acf_add_options_page') ) { 
+        acf_add_options_page(array(
+            'page_title' => 'Theme General Settings', 
+            'menu_title' => 'Theme Settings', 
+            'menu_slug'  => 'theme-general-settings', 
+            'capability' => 'edit_posts', 
+            'redirect'   => false
+        )); 
+    }
+});
 
 
 require_once get_template_directory() . '/inc/acf-options.php';
