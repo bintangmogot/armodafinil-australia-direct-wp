4ac140ce3bf9bc2304d8ae181478ba2fa8b550bb
bintangmogot <harunbintangmogot07@gmail.com>
2026-09-22 18:34:55 +0800

    Send site notifications to both inboxes

--- inc/email-routing.php
+++ inc/email-routing.php
@@ -10,8 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'AAD_OPERATIONS_EMAIL', 'Orders@armodafinildirect.com.au' );
-define( 'AAD_CONTACT_EMAIL', 'modaaust@gmail.com' );
+define( 'AAD_NOTIFICATION_EMAILS', 'Orders@armodafinildirect.com.au, modaaust@gmail.com' );
 
 // WooCommerce operational notifications are always delivered to Operations.
 foreach ( array(
@@ -21,14 +20,14 @@ foreach ( array(
 	'woocommerce_email_recipient_admin_payment_gateway_enabled',
 ) as $aad_email_filter ) {
 	add_filter( $aad_email_filter, function() {
-		return AAD_OPERATIONS_EMAIL;
+		return AAD_NOTIFICATION_EMAILS;
 	} );
 }
 
 // Gravity Forms contact notifications go to the dedicated contact inbox.
 add_filter( 'gform_notification', function( $notification, $form ) {
 	if ( in_array( (int) rgar( $form, 'id' ), array( 1, 2 ), true ) ) {
-		$notification['to'] = AAD_CONTACT_EMAIL;
+		$notification['to'] = AAD_NOTIFICATION_EMAILS;
 	}
 
 	return $notification;
